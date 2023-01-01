183ea98f1033967c48c3e61f603871879e7c60a1
Iamvibs <vm11082001@gmail.com>
2023-01-01 19:14:45 +0530

    first commit

---
+++ scripts/1_deploy.js
@@ -0,0 +1,35 @@
+async function main() {
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
+
+  // Fetch accounts
+  const accounts = await ethers.getSigners();
+  console.log(
+    `Account fetched:\n${accounts[0].address}\n${accounts[1].address}\n`
+  );
+
+  // Deploy contract
+  const VIBS = await Token.deploy("Vchain", "VIBS", "1000000");
+  await VIBS.deployed();
+  console.log(`VIBS Deployed to: ${VIBS.address}`);
+
+  const mETH = await Token.deploy("mETH", "mETH", "1000000");
+  await mETH.deployed();
+  console.log(`mETH Deployed to: ${mETH.address}`);
+
+  const mDAI = await Token.deploy("mDAI", "mDAI", "1000000");
+  await mDAI.deployed();
+  console.log(`mDAI Deployed to: ${mDAI.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10);
+  await exchange.deployed();
+  console.log(`Exchange Deployed to: ${exchange.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
